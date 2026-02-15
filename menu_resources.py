ca38df58efd0bfab64c0a4370f888030aed0f2c4
VY-Team-V <162020261+VY-Team-V@users.noreply.github.com>
2026-02-15 23:59:35 +0400

    Delete menu_resources.py

--- menu_resources.py
+++
@@ -1,9 +0,0 @@
-from settings import WIDTH, HEIGHT, HALF_WIDTH, HALF_HEIGHT
-
-MAPS_NAMES = ["Замок", "Лабиринт", "Военная база"]
-MAIN_MENU_ITEMS = ["Выбор карты", "Рекорды", "Выход"]
-
-# Цвета
-MENU_BG_COLOR = (20, 20, 40)
-MENU_TEXT_COLOR = (255, 255, 255)
-MENU_HIGHLIGHT_COLOR = (255, 50, 50)
